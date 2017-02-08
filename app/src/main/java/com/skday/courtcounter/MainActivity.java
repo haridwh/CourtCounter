d2e3895133fa90570a70849d238831563dc5e8e2
skday <haridwiharyono@gmail.com>
2017-02-08 17:05:50 +0700

    Change ic_launcher

--- app/src/main/java/com/skday/courtcounter/MainActivity.java
+++ app/src/main/java/com/skday/courtcounter/MainActivity.java
@@ -3,7 +3,6 @@ package com.skday.courtcounter;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
